4356035fc38572eebac19efdb925b828a00292e4
Pixel <louis.vincent.bompart@gmail.com>
2017-02-03 14:58:39 -0500

    Minor fixes
    1/ Init srand only once now
    2/ Cases can only have one jewel and one dust
    3/ Jewel are now properly added

--- 8INF846-1/8INF846-1/Environnement.cpp
+++ 8INF846-1/8INF846-1/Environnement.cpp
@@ -5,34 +5,35 @@
 
 Environnement::Environnement()
 {
-    int taille = 5;
-    CaseEnvironnement* aCase;
-    std::vector<CaseEnvironnement*> ligne;
+	int taille = 5;
+	CaseEnvironnement* aCase;
+	std::vector<CaseEnvironnement*> ligne;
+	srand(time(NULL));
 
-    for (unsigned int i = 0; i < taille; i++) {
-        for (unsigned int j = 0; j < taille; j++) {
+	for (unsigned int i = 0; i < taille; i++) {
+		for (unsigned int j = 0; j < taille; j++) {
 			aCase = new CaseEnvironnement();
 			allRooms.push_back(aCase);
 			ligne.push_back(aCase);
-        }
-        cases.push_back(ligne);
-        ligne.clear();
-    }
+		}
+		cases.push_back(ligne);
+		ligne.clear();
+	}
 	std::vector<CaseEnvironnement*> adjRooms;
 	for (int i = 0; i < taille; i++) {
 		for (unsigned int j = 0; j < taille; j++) {
 			adjRooms = cases[i][j]->AdjacentRooms();
 			if (i < taille - 1) {
-				adjRooms.push_back(cases[i + 1 ][j]);
+				adjRooms.push_back(cases[i + 1][j]);
 			}
 			if (j < taille - 1) {
-				adjRooms.push_back(cases[i][j +1]);
+				adjRooms.push_back(cases[i][j + 1]);
 			}
 			if (j > 0) {
 				adjRooms.push_back(cases[i][j - 1]);
 			}
 			if (i > 0) {
-				adjRooms.push_back(cases[i-1][j]);
+				adjRooms.push_back(cases[i - 1][j]);
 			}
 			cases[i][j]->AdjacentRooms(adjRooms);
 		}
@@ -43,18 +44,17 @@ Environnement::Environnement()
 void Environnement::Execute()
 {
 	while (true) {
-		srand(time(NULL));
 		int random = rand() % 3;
 		int randomDust;
 		int randomJewels;
 		for (int i = 0; i < random; i++) {
 			randomDust = rand() % allRooms.size();
-			allRooms[randomDust]->Poussiere(allRooms[randomDust]->Poussiere() + 1);
+			allRooms[randomDust]->Poussiere(1);
 		}
-		random = rand() % 1;
+		random = rand() % 2;
 		for (int i = 0; i < random; i++) {
 			randomJewels = rand() % allRooms.size();
-			allRooms[randomJewels]->Poussiere(allRooms[randomDust]->Poussiere() + 1);
+			allRooms[randomJewels]->Jewels(1);
 		}
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	}
